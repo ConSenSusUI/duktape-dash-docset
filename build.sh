f82e934ac8d65188fe0b7fc0d882ca070e00fc3c
Ramses Ladlani <ramses.ladlani@fishingcactus.com>
2015-09-02 18:46:24 +0200

    copy stylesheet into docset and generate archive

--- build.sh
+++ build.sh
@@ -2,7 +2,11 @@
 
 curl -O http://duktape.org/api.html
 curl -O http://duktape.org/style.css
+
 dashing build duktape -s ./api.html
+
+cp style.css duktape.docset/Contents/Resources/Documents/style.css
+
 sqlite3 -batch duktape.docset/Contents/Resources/docSet.dsidx << 'EOF'
   /*remove parenthesis and extra .(dot) in front of function names (used to help searching the webpage online)*/
   update searchIndex set name=substr(name,2,length(name)-3) where type="Function";
@@ -10,3 +14,5 @@ sqlite3 -batch duktape.docset/Contents/Resources/docSet.dsidx << 'EOF'
   /*remove paragraph sign after guide name*/
   update searchIndex set name=substr(name,1,length(name)-2) where type="Guide";
 EOF
+
+tar --exclude='.DS_Store' -cvzf duktape.tgz duktape.docset
